25aa2dd74c79b3e0b2e940cf8a692ce8c95ccea5
Firuddin <Firuddinaliyev888@gmail.com>
2024-08-27 01:35:32 +0400

    add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';  
+
+function App() {
+  return (
+    <>
+      <p className="text-xl font-bold text-blue-500">Hello world</p>
+    </>
+  );
+}
+
+export default App;
