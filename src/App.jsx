ff278cdc707eeee0126d6b157c0bde856708f0e1
Firudin <Firuddinaliyev888@gmail.com>
2024-08-29 17:47:55 +0400

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './index.css';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import FeatureSection from './components/FeatureSection';
+import Workflow from './components/workflow';
+import Pricing from './components/Pricing';
+
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
      <div className="max-w-7xl mx-auto pt-20 px-6">
        <HeroSection/>
        <FeatureSection/>
+       <Workflow/>
+       <Pricing/>
      </div>
      
     </>
