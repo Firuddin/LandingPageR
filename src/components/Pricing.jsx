28bcb100066e869f28092cc0fb4377581012d81e
Firuddin <firuddinaliyev888@gmail.com>
2024-08-31 11:43:05 +0400

    add the end

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -31,7 +31,7 @@ const Pricing = () => {
                          </li>
                       ))}
                      </ul>
-                     <a href="#" className="inline-flex justify-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl  hover:bg-orange-900 border border-orange-900 rounded-lg transition duration-200 pb-10">Subscribe</a>
+                     <a href="#" className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl  hover:bg-orange-900 border border-orange-900 rounded-lg transition duration-200 ">Subscribe</a>
                 </div>
             </div>
           ))}
